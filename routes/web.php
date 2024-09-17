b0f328e9d514ca2353399bae155f6f76e71a3493
unknown <you@example.com>
2024-09-17 13:37:32 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('employees', EmployeeController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
